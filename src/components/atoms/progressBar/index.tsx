4916f147672ac16cac5d2f3ce270db0a192ee9e9
shimon <69175188+lil-shimon@users.noreply.github.com>
2021-10-27 11:46:41 +0900

    feat(progress): add progress component

--- src/components/atoms/progressBar/index.tsx
+++ src/components/atoms/progressBar/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import { Box, LinearProgress, Typography } from '@material-ui/core'
+import React, { useEffect, useRef, useState } from 'react'
 
 /**
  * LinearProgressWithLabelProps定義
@@ -8,14 +9,65 @@ export interface LinearProgressWithLabelProps {
 }
 
 /**
+ * Props定義
+ */
+export interface Props {
+  isStart: boolean
+}
+
+/**
  * LinearProgressとlabelコンポーネント
  * @param {number} percent progressの数字
  * @returns 
  */
-export const LinearProgressWithLabel: React.FC<LinearProgressWithLabelProps> = ({percent}) => {
+const LinearProgressWithLabel: React.FC<LinearProgressWithLabelProps> = (props) => {
     return (
-      <div>
-        {percent}
-      </div>
+      <Box display="flex" alignItems="center">
+        <Box width="100%" mr={1}>
+          <LinearProgress variant="determinate" {...props}/>
+        </Box>
+        <Box minWidth={35}>
+          <Typography variant="body2" color="textSecondary">
+            {`${Math.round(props.percent)}%`}
+          </Typography>
+        </Box>
+      </Box>
     )
+}
+
+export const LinearProgressBarWithLabel: React.FC<Props> = ({ isStart }) => {
+  const [progress, setProgress] = useState<number>(10);
+  const ref = useRef<NodeJS.Timer | null>(null)
+
+  /**
+   * progressをスタートさせる関数
+   */
+  const start = () => {
+    ref.current = setInterval(() => {
+      setProgress((prev: number) => checkProgress(prev))
+    }, 1100)
+  }
+
+  const stop = () => {
+    ref.current && clearInterval(ref.current);
+    ref.current = null;
+  }
+
+  const checkProgress = (progress: number) => {
+    return progress <= 90 ? progress + 10 : progress;
+  }
+
+  useEffect(() => {
+    isStart && progress < 99 && start();
+  }, [isStart])
+
+  useEffect(() => {
+    progress > 99 && stop();
+  }, [progress])
+
+  return (
+    <div>
+      <LinearProgressWithLabel percent={progress} />
+    </div>
+  )
 }
---
+++ src/components/atoms/progressBar/styles.ts
@@ -0,0 +1,14 @@
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+    alignItems: "center"
+  },
+  progress: {
+    width: "100%",
+    marginRight: "1rem"
+  }
+})
+
+export default useStyles;
